e3b6a72ade9d0e84401204a19b7d50153c924a23
ding <dzhongyue@gmail.com>
2017-10-27 16:30:16 +0800

    test

---
+++ src/ding/github/Hello.java
@@ -0,0 +1,10 @@
+package ding.github;
+
+/**
+ * Created by ding on 17-10-27.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("hello, my first project in github by idea");
+    }
+}
